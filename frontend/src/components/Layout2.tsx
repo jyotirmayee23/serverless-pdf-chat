ee4ef8b8427fc3f2ef0f2ad2ba6879a2902434a3
jyotirmayee23 <156336518+jyotirmayee23@users.noreply.github.com>
2024-02-12 18:02:00 +0530

    chat textbox -flow according to the original

--- frontend/src/components/Layout2.tsx
+++ frontend/src/components/Layout2.tsx
@@ -2,29 +2,31 @@ import React, { useState, useEffect } from 'react'
 import DocumentsList2 from './DocumentsList2';
 import ChatBox from './ChatBox';
 import { API } from "aws-amplify";
-
-
+ 
+ 
 function Layout2() {
   const storedBoardId = sessionStorage.getItem('boardId');
   // console.log("s boardid", storedBoardId);
   const [files, setFiles] = useState([])
-  const [showMain, setShowMain] = useState(true);
   const [docs, setDocs] = useState([]);
   const [conversation, setConversation] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [messageStatus, setMessageStatus] = useState("");
-
+  const [messageStatus, setMessageStatus] = useState < string > ("idle");
+  const [prompt, setPrompt] = useState("");
+  const [loading, setLoading] = React.useState < string > ("idle");
+ 
+ 
+ 
   useEffect(() => {
     fetchData();
   }, [storedBoardId]);
-
+ 
   useEffect(() => {
     getAllFiles();
   }, [])
-
+ 
   const fetchData = () => {
     let query = `query { boards (ids: ${storedBoardId}) {items_page (limit: 100) { items { assets {id name public_url }}}}}`
-
+ 
     fetch("https://api.monday.com/v2", {
       method: 'post',
       headers: {
@@ -38,13 +40,13 @@ function Layout2() {
       .then(res => res.json())
       .then(res => {
         if (res.data && res.data.boards) {
-          const fileDetails = res.data.boards[0].items_page.items.flatMap(item =>
-            item.assets.map(asset => {
+          const fileDetails = res.data.boards[0].items_page.items.flatMap((item: { assets: any[]; }) =>
+            item.assets.map((asset: { public_url: any; id: any; name: any; }) => {
               const fileUrl = asset.public_url;
               return { id: asset.id, public_url: fileUrl, name: asset.name };
             }),
           );
-
+ 
           if (fileDetails) {
             setFiles(fileDetails);
           }
@@ -52,33 +54,65 @@ function Layout2() {
       })
       .catch(error => console.error('Error:', error));
   };
-
+ 
   const getAllFiles = async () => {
     const documents = await API.get("serverless-pdf-chat", "/doc", {});
     if (documents.length > 0) {
       setDocs(documents);
     }
   };
-  const handlestartchatParent = async (documentid, conversationid) => {
-    const fetchData = async (docid, convid) => {
+  const handlestartchatParent = async (documentid: any, conversationid: any) => {
+    const fetchData = async (docid: any, convid: any) => {
       try {
         const conversationData = await API.get('serverless-pdf-chat', `/doc/${docid}/${convid}`, {}
         );
         setConversation(conversationData);
-        setLoading(false);
         // setIdle(true)
       } catch (error) {
         console.error('Error fetching conversation:', error);
       }
     };
-
+ 
     fetchData(documentid, conversationid);
   };
-
-  const handleButtonClick = async (conversationId, filename, input, documentId) => {
-    setMessageStatus("idle")
-    setLoading(true);
-
+ 
+ 
+  const getAllChatData = async (documentid: any, conversationid: any) => {
+    setLoading("loading");
+    const fetchData = async (docid: any, convid: any) => {
+      try {
+        const conversationData = await API.get('serverless-pdf-chat', `/doc/${docid}/${convid}`, {}
+        );
+        setConversation(conversationData);
+        setLoading("idle");
+      } catch (error) {
+        console.error('Error fetching conversation:', error);
+      }
+    };
+ 
+    fetchData(documentid, conversationid);
+  };
+ 
+  const submitMessage = async (conversationId: any, filename: any, prompt: any, documentId: any) => {
+    setMessageStatus("loading");
+    if (conversation !== null) {
+      const previewMessage = {
+        type: "text",
+        data: {
+          content: prompt,
+          additional_kwargs: {},
+          example: false,
+        },
+      };
+ 
+      const updatedConversation = {
+        ...conversation,
+        messages: [...conversation.messages, previewMessage],
+      };
+ 
+      setConversation(updatedConversation);
+    }
+ 
     try {
       const response = await API.post(
         "serverless-pdf-chat",
@@ -86,44 +120,56 @@ function Layout2() {
         {
           body: {
             fileName: filename,
-            prompt: input,
+            prompt: prompt,
           },
         }
       );
-      console.log("API Response:", response);
-      handlestartchatParent(documentId, conversationId);
-
+      // console.log("API Response:", response);
+      setPrompt("");
+      getAllChatData(documentId, conversationId);
+      setMessageStatus("idle");
+ 
     } catch (error) {
       console.error("API Error:", error);
     }
-
+ 
   }
-
-
-
-
-
+ 
+  const handleKeyPress = (event, conversationId, filename, prompt, documentId) => {
+    console.log("func called")
+    if (event.key === "Enter") {
+      submitMessage(conversationId, filename, prompt, documentId);
+    }
+  }
+ 
+  const handlePromptChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setPrompt(event.target.value);
+  };
+ 
   return (
-    <div className="main_container">
-      <div className="files_con">
-        <DocumentsList2
-          fileData={files}
-          handlestartchatParent={handlestartchatParent}
-          documents={docs}
-        />
+    <>
+      <div className="main_container">
+        <div className="files_con">
+          <DocumentsList2
+            fileData={files}
+            handlestartchatParent={handlestartchatParent}
+            documents={docs}
+          />
+        </div>
+        <div className='chat_con'>
+          <ChatBox
+            handleKeyPress={handleKeyPress}
+            prompt={prompt}
+            conversation={conversation}
+            submitMessage={submitMessage}
+            loading={loading}
+            messageStatus={messageStatus}
+            handlePromptChange={handlePromptChange}
+          />
+        </div>
       </div>
-      <div className='chat_con'>
-        <ChatBox
-          // handleKeyPress={handleKeyPress}
-          showMain={showMain}
-          conversation={conversation}
-          onButtonClick={handleButtonClick}
-          loading={loading}
-          messageStatus={messageStatus}
-        />
-      </div>
-    </div>
+    </>
   )
 }
-
+ 
 export default Layout2;
