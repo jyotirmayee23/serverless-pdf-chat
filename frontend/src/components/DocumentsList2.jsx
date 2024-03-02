c3c3698403cacbeaa6829cbcc6b0b79d414ce526
jyotirmayee23 <156336518+jyotirmayee23@users.noreply.github.com>
2024-03-02 14:58:59 +0530

    Update DocumentsList2.jsx

--- frontend/src/components/DocumentsList2.jsx
+++ frontend/src/components/DocumentsList2.jsx
@@ -169,7 +169,7 @@ function DocumentsList2({
               </div>
               <div className='py-[5px] px-3  w-[45%]'>
                 <div className=''>
-                  <p className='file_name' data-tooltip-id="my-tooltip-1"}>{file.name}</p>
+                  <p className='file_name' data-tooltip-id="my-tooltip-1">{file.name}</p>
                   <ReactTooltip id="my-tooltip-1" place="bottom" content="Hello world! I'm a Tooltip"/>
                 </div>
                 {file.meta === 'meta' && (
